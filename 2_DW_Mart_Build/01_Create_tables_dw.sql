e743ce16005a69c231ea2a8baa9e2a02c8db46a9
Suman Dey <sumandey504@gmail.com>
2026-07-01 00:35:33 +0530

    Create star schema tables for DW

---
+++ 2_DW_Mart_Build/01_Create_tables_dw.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS company_dim(
+    company_id  INT PRIMARY KEY,
+    name        VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS skills_dim(
+    skill_id    INT PRIMARY KEY,
+    skill       VARCHAR,
+    type        VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS job_postings_fact (
+    job_id                  INT PRIMARY KEY,
+    company_id              INT,
+    job_title_short         VARCHAR,
+    job_title               VARCHAR,
+    job_location            VARCHAR,
+    job_via                 VARCHAR,
+    job_schedule_type       VARCHAR,
+    job_work_from_home      BOOLEAN,
+    search_location         VARCHAR,
+    job_posted_date         TIMESTAMP,
+    job_no_degree_mention   BOOLEAN,
+    job_health_insurance    BOOLEAN,
+    job_country             VARCHAR,
+    salary_rate             VARCHAR,
+    salary_year_avg         DOUBLE,
+    salary_hour_avg         DOUBLE,
+    FOREIGN KEY(company_id) REFERENCES company_dim(company_id)
+
+);
+
+CREATE TABLE IF NOT EXISTS skills_job_dim(
+    skill_id    INT,
+    job_id      INT,
+    PRIMARY KEY(skill_id,job_id),
+    FOREIGN KEY (skill_id) REFERENCES skills_dim(skill_id),
+    FOREIGN KEY (job_id) REFERENCES job_postings_fact(job_id)
+);
+
+SHOW TABLES;
